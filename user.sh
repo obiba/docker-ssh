40260826860f149aed9b142903bd11ba562ddfad
Yannick Marcon <yannick.marcon@obiba.org>
2020-04-28 16:12:18 +0000

    code cleaning

--- user.sh
+++ user.sh
@@ -7,13 +7,6 @@ useradd -m -d /home/${SSH_MASTER_USER} -G ssh ${SSH_MASTER_USER} -s /bin/bash
 echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
 echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
  
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
-#echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
- 
 addgroup sftp
  
 exec "$@"
